db78aee11f6dcc48f1a5312c58ec05309a5cefdc
tingshuai <1085370797@qq.com>
2018-10-24 14:02:42 +0800

    1.0.5

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ ui.extend({
   http(obj){
     let that = this;
     let a = ui.getApp().globalData.token;
-    debugger;
+    ui.showLoading();
     ui.request({
       url: `${obj.url}`, //仅为示例，并非真实接口地址。
       method:obj.method || "POST",
@@ -17,18 +17,21 @@ ui.extend({
         "content-type" : obj.contentType || "application/json"
       },
       success: (res) => {
+        // prompt("成功----",JSON.stringify(res))
+        ui.hideLoading()
         if( res.data.code == 203 ){//token失效....
-          debugger;
           ui.navigateTo({
             url:`/pages/login`
           })
         }else if( res.data.code == 200 ){//成功......
           obj.scb(res)
-        }else if( res.data.code == 401 ){//返回提示.....
+        }else if( res.data.code == 401 || res.data.code == 501){//返回提示.....
           ui.showToast({ title: res.data.msg, icon: 'none' ,duration:2000})
         }
       },
       fail(res){
+        ui.hideLoading()
+        // prompt("失败----",JSON.stringify(res))
         obj.fcb(res)
       }
     });
